a80dda18938f8749910d9d9a4df59055bd3fe4b6
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-07-07 02:29:29 -0400

    added some more descriptive console logs

--- js/index.js
+++ js/index.js
@@ -8,12 +8,15 @@ menu.onchange = function () {
     document.body.classList.remove(appliedFix);
     appliedFix = menu.selectedOptions[0].value;
     document.body.classList.add(appliedFix);
+    console.log("added " + appliedFix + "class to document body");
 }
 
 checkbox.onchange = function () {
     if (checkbox.checked) {
         document.body.classList.remove("hide-arrows");
+        console.log("removed hide-arrows class from document body");
     } else {
         document.body.classList.add("hide-arrows");
+        console.log("added hide-arrows class to document body");
     }
 }
