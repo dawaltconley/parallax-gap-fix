ea0e9bf54c0f35b8c82d3005c1dc18ce930158a6
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-07-07 01:24:34 -0400

    updated, much prettier

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 window.onload = function () {
   var appliedFix = null;
   var menu = document.getElementById("fix-options");
+  var checkbox = document.getElementById("toggle-center");
+
+  document.body.classList.add("hide-arrows");
+
   menu.onchange = function () {
     document.body.classList.remove(appliedFix);
     appliedFix = menu.selectedOptions[0].value;
     document.body.classList.add(appliedFix)
+  }
+  
+  checkbox.onchange = function () {
+    if (checkbox.checked) {
+      document.body.classList.remove("hide-arrows");
+    } else {
+      document.body.classList.add("hide-arrows");
+    }
   }
 }
